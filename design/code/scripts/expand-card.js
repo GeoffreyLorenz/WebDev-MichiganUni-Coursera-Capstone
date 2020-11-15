e3c395238b2bd99dd1be3cf293338eb1205d659a
Geoffrey Lorenz <geoffrey.lrz@gmail.com>
2020-11-15 19:46:36 +0100

    fix: solved the card display issue

--- design/code/scripts/expand-card.js
+++ design/code/scripts/expand-card.js
@@ -1,6 +1,6 @@
 Array.from(document.querySelectorAll('.see-more-icon'), function (elem) {
     elem.addEventListener('click', function hideContent(e) {
-        e.target.nextSibling.querySelector('.card-text')
+        e.currentTarget.parentNode.querySelector('.card-text')
                        .classList.toggle('visible');
     });
 });
