730f7faf47cc8d5879251f8987474f94e505e9fe
Geoffrey Lorenz <geoffrey.lrz@gmail.com>
2020-11-17 00:15:46 +0100

    feature: created transition onclick for see-more-icon
    Don´t have time to code further, so commit for now

--- design/code/scripts/expand-card.js
+++ design/code/scripts/expand-card.js
@@ -2,5 +2,8 @@ Array.from(document.querySelectorAll('.see-more-icon'), function (elem) {
     elem.addEventListener('click', function hideContent(e) {
         e.currentTarget.parentNode.querySelector('.card-text')
                        .classList.toggle('visible');
+        e.currentTarget.parentNode.querySelector('.see-more-icon')
+                       .classList.toggle('spin');
     });
+
 });
